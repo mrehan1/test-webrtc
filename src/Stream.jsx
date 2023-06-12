9c5712de017dfb5684b41e116060d389c188a217
Muhammad Rehan Qureshi <rehan.qureshi@onepeloton.com>
2023-06-12 02:54:50 -0700

    Update stuff.

--- src/Stream.jsx
+++ src/Stream.jsx
@@ -9,10 +9,16 @@ export const AppStreamCam = () => {
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then((mediaStream) => {
+        console.log("hello")
+        console.log(mediaStream);
         const video = videoRef.current;
 
         video.srcObject = mediaStream;
-        video.onloadedmetadata = () => video.play();
+        const track = mediaStream.getTracks()[0];
+        console.log(track.getSettings());
+        video.play();
+
+        console.log(video)
       })
       .catch(function (err) {
         console.log(err.name + ": " + err.message);
@@ -24,13 +30,17 @@ export const AppStreamCam = () => {
     console.log(devices)
   }
   useEffect(() => {
-    streamCamVideo();
     getDevices();
   }, [])
 
   return (
-    <div style={{ "width": "1280px", "height": "720px" }}>
-      <video autoPlay={true} id="audioElement" style={{ "width": "1280px", "height": "720px" }} ref={videoRef} ></video>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ "width": "1280px", "height": "720px" }}>
+        <video autoPlay={true} id="audioElement" style={{ "width": "1280px", "height": "720px" }} ref={videoRef} src="/video.mp4" loop></video>
+      </div>
+      {/* <button onClick={streamCamVideo}> */}
+      {/*   Play */}
+      {/* </button> */}
     </div>
   );
 }
